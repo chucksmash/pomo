c8e994339ea5fb4bd949f0c8ac1fc64e0ecd2490
Chuck Bassett <3101367+chucksmash@users.noreply.github.com>
2018-08-21 16:42:26 -0400

    Some cleanup + Timer enum

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 extern crate chrono;
 extern crate termion;
 
+use std::default::Default;
 use std::fmt;
 use std::io::{self, Read, Write};
+use std::ops::{Deref, DerefMut};
 use std::thread::sleep;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 use chrono::{DateTime, Duration as OldDuration, Local};
 use termion::{async_stdin, clear, color, cursor, style};
 use termion::raw::IntoRawMode;
 use termion::screen::{self, AlternateScreen};
 
-use self::timer::{Countdown, Position};
+use self::timer::{Countdown, Position, Timer};
 
 macro_rules! maybe_val {
     ( $val:expr, $test:expr, $label:expr ) => {
@@ -46,21 +48,15 @@ const SEC_IN_HOUR: u64 = SEC_IN_MINUTE * 60;
 const SLEEP: Duration = Duration::from_millis(100);
 
 struct Pomodoro<R, W> {
-    current: Countdown,
-    previous: Vec<Countdown>,
-    run_length: Duration,
-    break_length: Duration,
+    current: Timer,
     stdin: R,
     stdout: W,
 }
 
 impl<R: Read, W: Write> Pomodoro<R, W> {
-    fn new(stdin: R, stdout: W) -> Pomodoro<R, W> {
+    fn new(stdin: R, stdout: W, timer: Timer) -> Pomodoro<R, W> {
         Pomodoro {
-            current: Countdown::new(Duration::from_secs(0), ""),
-            previous: vec![],
-            run_length: Duration::from_secs(10),
-            break_length: Duration::from_secs(5),
+            current: timer,
             stdin,
             stdout,
         }
@@ -73,7 +69,8 @@ impl<R: Read, W: Write> Pomodoro<R, W> {
                  cursor::Hide,
                  cursor::Goto(1, 1))?;
 
-        self.current = Countdown::new(self.run_length, "Whatever");
+        let duration = Duration::from_secs(1500);
+        self.current = Timer::Work(Countdown::new(duration, "Parsing HH:MM:SS"));
 
         while timer::State::Finished != self.current.tick() {
             let mut key_bytes = [0];
@@ -89,9 +86,9 @@ impl<R: Read, W: Write> Pomodoro<R, W> {
             sleep(SLEEP);
         }
         self.cleanup()?;
-        write!(self.stdout,
-               "{}\r\n",
-               self.current.summarize().replace("\n", "\r\n"))?;
+        // write!(self.stdout,
+        //        "{}\r\n",
+        //        self.current.summarize().replace("\n", "\r\n"))?;
         Ok(())
     }
 
@@ -137,7 +134,6 @@ mod timer {
         running: Duration,
         paused: Duration,
         title: String,
-        note: Option<String>,
     }
 
     impl Countdown {
@@ -149,7 +145,6 @@ mod timer {
                 running: Duration::from_secs(0),
                 paused: Duration::from_secs(0),
                 title: String::from(title),
-                note: None,
             }
         }
 
@@ -180,62 +175,82 @@ mod timer {
         pub fn total(&self) -> Duration {
             self.running + self.paused
         }
+    }
 
-        pub fn summarize(&self) -> String {
-            let total = self.total();
-            let end = self.start + OldDuration::from_std(total).unwrap();
-            format!("{} {}
-  - Total Duration: {}
-  - Time Running:   {}
-  - Time Paused:    {}
-  - Started:        {}
-  - Ended:          {}",
-                    self.title, self.state,
-                    format_duration(&total, &total),
-                    format_duration(&self.running, &total),
-                    format_duration(&self.paused, &total),
-                    self.start.to_rfc3339(),
-                    end.to_rfc3339())
+    // impl fmt::Display for Countdown {
+    //     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    //         use self::State::*;
 
+    //         let duration = match self.duration.checked_sub(self.running) {
+    //             Some(elapsed) => elapsed,
+    //             None => self.duration,
+    //         };
+
+    //         let status = match self.state {
+    //             Finished => "[FINISHED]",
+    //             Paused => "[PAUSED]",
+    //             Running => "",
+    //         };
+    //         write!(f, "{} {}", format_duration(&duration, &self.duration), status)
+    //     }
+    // }
+
+    impl Default for Countdown {
+        fn default() -> Countdown {
+            Countdown::new(Duration::from_secs(0), "")
         }
     }
 
-    impl fmt::Display for Countdown {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            use self::State::*;
+    pub enum Timer {
+        Work(Countdown),
+        Break(Countdown),
+    }
 
-            let duration = match self.duration.checked_sub(self.running) {
-                Some(elapsed) => elapsed,
-                None => self.duration,
-            };
+    impl Deref for Timer {
+        type Target = Countdown;
 
-            let status = match self.state {
-                Finished => "[FINISHED]",
-                Paused => "[PAUSED]",
-                Running => "",
-            };
-            write!(f, "{} {}", format_duration(&duration, &self.duration), status)
+        fn deref(&self) -> &Self::Target {
+            match self {
+                Timer::Work(c) => &c,
+                Timer::Break(c) => &c,
+            }
         }
     }
 
-    /// Duration isn't our struct and Display isn't our trait
-    /// so the majority of this cannot go into an
-    /// `impl Display for Duration` as I originally intended.
-    fn format_duration(curr: &Duration, out_of: &Duration) -> String {
-        let max = out_of.as_secs();
-        let total = curr.as_secs();
-        let mut tmp = total;
-        let hours = tmp / SEC_IN_HOUR;
-        tmp %= SEC_IN_HOUR;
-        let minutes = tmp / SEC_IN_MINUTE;
-        tmp %= SEC_IN_MINUTE;
-        let seconds = tmp;
-
-        format!("{}{:02}:{:02}",
-                maybe_val!(hours, max >= SEC_IN_HOUR, ":"),
-                minutes,
-                seconds)
+    impl DerefMut for Timer {
+        fn deref_mut(&mut self) -> &mut Countdown {
+            match self {
+                Timer::Work(ref mut c) => c,
+                Timer::Break(ref mut c) => c,
+            }
+        }
     }
+
+    impl Default for Timer {
+        fn default() -> Timer {
+            Timer::Work(Default::default())
+        }
+    }
+
+
+    // /// Duration isn't our struct and Display isn't our trait
+    // /// so the majority of this cannot go into an
+    // /// `impl Display for Duration` as I originally intended.
+    // fn format_duration(curr: &Duration, out_of: &Duration) -> String {
+    //     let max = out_of.as_secs();
+    //     let total = curr.as_secs();
+    //     let mut tmp = total;
+    //     let hours = tmp / SEC_IN_HOUR;
+    //     tmp %= SEC_IN_HOUR;
+    //     let minutes = tmp / SEC_IN_MINUTE;
+    //     tmp %= SEC_IN_MINUTE;
+    //     let seconds = tmp;
+
+    //     format!("{}{:02}:{:02}",
+    //             maybe_val!(hours, max >= SEC_IN_HOUR, ":"),
+    //             minutes,
+    //             seconds)
+    // }
 
     #[derive(Debug, PartialEq)]
     pub struct Position {
@@ -295,17 +310,17 @@ mod timer {
                           "█████",
                           "    █",
                           "█████"],
-            ":" => lines!["     ",
-                          "  █  ",
-                          "     ",
-                          "  █  ",
-                          "     "],
+            ":" => lines!["   ",
+                          " █ ",
+                          "   ",
+                          " █ ",
+                          "   "],
             " " => (0..5).map(|_| " ".to_owned()).collect::<Vec<_>>(),
             _ => (0..5).map(|_| "".to_owned()).collect::<Vec<_>>(),
         }
     }
 
-    fn to_digits(countdown: &Countdown) -> Vec<String> {
+    fn to_digit_strs(countdown: &Countdown) -> Vec<String> {
         if let Some(left) = countdown.duration.checked_sub(countdown.running) {
             let total = countdown.duration.as_secs();
             let mut tmp = left.as_secs();
@@ -342,7 +357,7 @@ mod timer {
         let mut lines: Vec<Vec<String>> = (2..7)
             .map(|idx| vec![format!("{}", cursor::Goto(pos.x, pos.y + idx))])
             .collect();
-        let digits = to_digits(countdown);
+        let digits = to_digit_strs(countdown);
         for digit in &digits {
             let mut digit_lines = render_digit(digit);
             for i in (0..5).rev() {
@@ -366,8 +381,8 @@ mod timer {
 
 fn main() {
     let stdout = io::stdout();
-    let mut screen = AlternateScreen::from(
+    let screen = AlternateScreen::from(
         stdout.lock().into_raw_mode().unwrap());
-    let mut pomo = Pomodoro::new(async_stdin(), screen);
+    let mut pomo = Pomodoro::new(async_stdin(), screen, Default::default());
     pomo.run().unwrap();
 }

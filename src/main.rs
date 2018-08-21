f902a9502d9368b4d2255b10299fb37290670480
Chuck Bassett <3101367+chucksmash@users.noreply.github.com>
2018-08-21 13:49:04 -0400

    Little clean up

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,30 @@ use termion::{async_stdin, clear, color, cursor, style};
 use termion::raw::IntoRawMode;
 use termion::screen::{self, AlternateScreen};
 
-use self::timer::Countdown;
+use self::timer::{Countdown, Position};
 
 macro_rules! maybe_val {
     ( $val:expr, $test:expr, $label:expr ) => {
         {
             match $test {
-                true => format!("{} {}", $val, $label),
+                true => format!("{:02}{}", $val, $label),
                 false => "".to_owned(),
             }
+        }
+    }
+}
+
+macro_rules! lines {
+    ( $( $line:expr ),* ) => {
+        {
+            let mut tmp = Vec::new();
+            let blue = color::Fg(color::Rgb(0x26, 0x8b, 0xd2));
+            $(
+                tmp.push(
+                    format!("{reset}{color}{line}{reset}",
+                            reset=style::Reset, color=blue, line=$line));
+            )*
+            tmp
         }
     }
 }
@@ -28,8 +43,6 @@ type TermResult = Result<(), io::Error>;
 
 const SEC_IN_MINUTE: u64 = 60;
 const SEC_IN_HOUR: u64 = SEC_IN_MINUTE * 60;
-const SEC_IN_DAY: u64 = SEC_IN_HOUR * 24;
-const DEFAULT_DURATION: Duration = Duration::from_secs(10);
 const SLEEP: Duration = Duration::from_millis(100);
 
 struct Pomodoro<R, W> {
@@ -211,36 +224,149 @@ mod timer {
         let max = out_of.as_secs();
         let total = curr.as_secs();
         let mut tmp = total;
-        let days = tmp / SEC_IN_DAY;
-        tmp %= SEC_IN_DAY;
         let hours = tmp / SEC_IN_HOUR;
         tmp %= SEC_IN_HOUR;
         let minutes = tmp / SEC_IN_MINUTE;
         tmp %= SEC_IN_MINUTE;
         let seconds = tmp;
 
-        format!("{}{}{}{:02} seconds",
-                maybe_val!(days, max >= SEC_IN_DAY, "days "),
-                maybe_val!(hours, max >= SEC_IN_HOUR, "hours "),
-                maybe_val!(minutes, max >= SEC_IN_MINUTE, "minutes "),
+        format!("{}{:02}:{:02}",
+                maybe_val!(hours, max >= SEC_IN_HOUR, ":"),
+                minutes,
                 seconds)
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct Position {
+        pub x: u16,
+        pub y: u16,
+    }
+
+    fn render_digit(digit: &str) -> Vec<String> {
+        match digit {
+            "0" => lines!["█████",
+                          "█   █",
+                          "█   █",
+                          "█   █",
+                          "█████"],
+            "1" => lines!["    █",
+                          "    █",
+                          "    █",
+                          "    █",
+                          "    █"],
+            "2" => lines!["█████",
+                          "   ██",
+                          "█████",
+                          "█    ",
+                          "█████"],
+            "3" => lines!["█████",
+                          "    █",
+                          "█████",
+                          "    █",
+                          "█████"],
+            "4" => lines!["█   █",
+                          "█   █",
+                          "█████",
+                          "    █",
+                          "    █"],
+            "5" => lines!["█████",
+                          "█    ",
+                          "█████",
+                          "    █",
+                          "█████"],
+            "6" => lines!["█████",
+                          "█    ",
+                          "█████",
+                          "█   █",
+                          "█████"],
+            "7" => lines!["█████",
+                          "    █",
+                          "    █",
+                          "    █",
+                          "    █"],
+            "8" => lines!["█████",
+                          "█   █",
+                          "█████",
+                          "█   █",
+                          "█████"],
+            "9" => lines!["█████",
+                          "█   █",
+                          "█████",
+                          "    █",
+                          "█████"],
+            ":" => lines!["     ",
+                          "  █  ",
+                          "     ",
+                          "  █  ",
+                          "     "],
+            " " => (0..5).map(|_| " ".to_owned()).collect::<Vec<_>>(),
+            _ => (0..5).map(|_| "".to_owned()).collect::<Vec<_>>(),
+        }
+    }
+
+    fn to_digits(countdown: &Countdown) -> Vec<String> {
+        if let Some(left) = countdown.duration.checked_sub(countdown.running) {
+            let total = countdown.duration.as_secs();
+            let mut tmp = left.as_secs();
+            let hours = tmp / 3600;
+            tmp %= 3600;
+            let minutes = tmp / 60;
+            tmp %= 60;
+            let seconds = tmp;
+            format!("{}{:02}:{:02}",
+                    maybe_val!(format!("{}:", hours), total >= SEC_IN_HOUR, ""),
+                    minutes,
+                    seconds)
+                .split("")
+                .collect::<Vec<&str>>()
+                .join(" ")
+                .split("")
+                .map(|c| String::from(c))
+                .collect::<Vec<String>>()
+        } else {
+            vec![]
+        }
+    }
+
+    pub fn render(countdown: &Countdown, pos: &Position) -> String {
+        let rendered_title = format!("{under}{bold}{title}",
+                                     under=style::Underline,
+                                     bold=style::Bold,
+                                     title=countdown.title);
+        let rendered_status = format!("{}",
+                                      match countdown.state {
+                                          State::Paused => "[PAUSED]",
+                                          _ => "",
+                                      });
+        let mut lines: Vec<Vec<String>> = (2..7)
+            .map(|idx| vec![format!("{}", cursor::Goto(pos.x, pos.y + idx))])
+            .collect();
+        let digits = to_digits(countdown);
+        for digit in &digits {
+            let mut digit_lines = render_digit(digit);
+            for i in (0..5).rev() {
+                lines[i].push(digit_lines.pop().unwrap());
+            }
+        }
+        let lines_str = lines
+            .into_iter()
+            .map(|line| line.join(""))
+            .collect::<Vec<String>>()
+            .join("");
+        format!("{clear}{pos}{reset}{title}{reset} {status}{lines}",
+                clear=clear::All,
+                pos=cursor::Goto(pos.x, pos.y),
+                reset=style::Reset,
+                title=rendered_title,
+                status=rendered_status,
+                lines=lines_str)
     }
 }
 
-// fn run() -> TermResult {
-//     let stdin = async_stdin();
-//     let stdout = io::stdout();
-//     let mut screen = AlternateScreen::from(stdout.lock().into_raw_mode()?);
-
-//     let mut pomo = Pomodoro::new(stdin, screen);
-//     pomo.run()
-// }
-
 fn main() {
-    let stdin = async_stdin();
     let stdout = io::stdout();
     let mut screen = AlternateScreen::from(
         stdout.lock().into_raw_mode().unwrap());
-    let mut pomo = Pomodoro::new(stdin, screen);
+    let mut pomo = Pomodoro::new(async_stdin(), screen);
     pomo.run().unwrap();
 }
